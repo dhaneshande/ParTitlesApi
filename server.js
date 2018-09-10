47c69281800423d10aab51876371d270a4eb0df6
mbhasuri <mbhasuri@miraclesoft.com>
2018-09-10 11:24:41 -0400

    commit code by adding default route

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
+// adding middleware - cors
+app.use(cors());
+// adding middleware - body-parser
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
+app.get('/', (req, res, next) => {
+    res.send('Titles API');
+});
 app.listen(3000, () => { console.log('Server listening at port: 3000'); });
